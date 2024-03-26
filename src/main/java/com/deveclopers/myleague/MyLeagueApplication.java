affeaaf6e68d3e1087a94021a3b59c480d51d97a
Sergio Hidalgo <barcecheo2319@gmail.com>
2024-03-26 00:44:27 -0500

    init

---
+++ src/main/java/com/deveclopers/myleague/MyLeagueApplication.java
@@ -0,0 +1,13 @@
+package com.deveclopers.myleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLeagueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deveclopers/myleague/MyLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deveclopers.myleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
