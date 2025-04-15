446e598c94fe59f2d1be62ee74e3486d1f161bf0
Sergio Hidalgo <barcecheo2319@gmail.com>
2025-04-15 15:29:39 -0500

    Some changes for security

--- src/main/java/com/deveclopers/myleague/document/User.java
+++ src/main/java/com/deveclopers/myleague/document/User.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -11,7 +12,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Setter
 @AllArgsConstructor
 public class User {
+
   @Id private Long id;
+
+  @Indexed(unique = true)
   private String username;
+
   private String password;
 }
--- src/main/java/com/deveclopers/myleague/security/AuthTokenFilter.java
+++ src/main/java/com/deveclopers/myleague/security/AuthTokenFilter.java
@@ -5,17 +5,21 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.authentication.*;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import java.io.IOException;
 
 @Component
 public class AuthTokenFilter extends OncePerRequestFilter {
+  private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+
   @Autowired private JwtUtil jwtUtils;
   @Autowired private CustomUserDetailsService userDetailsService;
 
@@ -35,8 +39,10 @@ public class AuthTokenFilter extends OncePerRequestFilter {
         SecurityContextHolder.getContext().setAuthentication(authentication);
       }
     } catch (Exception e) {
-      System.out.println("Cannot set user authentication: " + e);
+      logger.error("Error al autenticar usuario: ", e);
+      response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
     }
+
     filterChain.doFilter(request, response);
   }
 
